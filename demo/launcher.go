02e6aa3f0b7158e50aa298fa8052a6ccc63a444a
liuxigang <liuxigang@jd.com>
2020-07-22 20:39:49 +0800

    demo-Major修改-Hi方法签名修改

--- demo/launcher.go
+++ demo/launcher.go
@@ -2,6 +2,10 @@ package demo
 
 import "fmt"
 
-func Hi(name *string) string {
+/*func Hi(name *string) string {
 	return fmt.Sprintf("hello, %s", *name)
+}*/
+
+func Hi(name *string) (string, uint32) {
+	return fmt.Sprintf("hello, %s", *name), nil
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/beaters/demo
 
 go 1.14
-
-require git.apache.org/thrift.git v0.13.0 // indirect
