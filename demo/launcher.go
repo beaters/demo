c1d92408627cfc593bb877e26c81d6c647cbbbda
liuxigang <liuxigang@jd.com>
2020-07-22 17:22:01 +0800

    demo-修改go module名称

--- demo/launcher.go
+++ demo/launcher.go
@@ -1,11 +1,6 @@
-package main
+package demo
 
 import "fmt"
-
-func main() {
-	fmt.Println("")
-
-}
 
 func Hi(name *string) string {
 	return fmt.Sprintf("hello, %s", name)
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/beaters/ThriftDemo
+module github.com/beaters/demo
 
 go 1.14
 
